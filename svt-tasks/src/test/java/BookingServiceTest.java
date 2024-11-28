7f015ec0b9a906843413578cbab6b94b50ee6e39
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-11-29 01:19:45 +0600

    Add files via upload

---
+++ svt-tasks/src/test/java/BookingServiceTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class BookingServiceTest {
+
+    @Mock
+    private PaymentService paymentService;
+
+
+    @InjectMocks
+    private BookingService bookingService;
+
+    @BeforeEach
+    void setUp() {
+
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void bookService() {
+        double bookingAmount = 100.0;
+
+        when(paymentService.processPayment(bookingAmount)).thenReturn(true);
+        
+        boolean paymentSuccess = bookingService.bookService(bookingAmount);
+
+        assertTrue(paymentSuccess);
+
+        verify(paymentService, times(1)).processPayment(bookingAmount);
+    }
+}
